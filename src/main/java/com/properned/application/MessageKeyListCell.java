799736d86c8812876e3a24400fae70f22db7713f
Alexandre NEDJARI <an@sylob.com>
2016-01-05 13:54:54 +0100

    #8 - add "Copy key fonctionnality"

--- src/main/java/com/properned/application/MessageKeyListCell.java
+++ src/main/java/com/properned/application/MessageKeyListCell.java
@@ -1,10 +1,18 @@
 package com.properned.application;
 
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.input.KeyCode;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import com.properned.model.MultiLanguageProperties;
 
@@ -29,6 +37,9 @@ import com.properned.model.MultiLanguageProperties;
  * @since 28 october 2015
  */
 public class MessageKeyListCell extends ListCell<String> {
+
+	private Logger logger = LogManager.getLogger(this.getClass());
+
 	MultiLanguageProperties properties;
 
 	public MessageKeyListCell(MultiLanguageProperties properties) {
@@ -45,8 +56,20 @@ public class MessageKeyListCell extends ListCell<String> {
 			setText(messageKey);
 
 			MenuItem deleteMenu = getDeleteMenu(messageKey);
+			MenuItem copyMenu = getCopyMenu(messageKey);
 
-			this.setContextMenu(new ContextMenu(deleteMenu));
+			this.setContextMenu(new ContextMenu(copyMenu,
+					new SeparatorMenuItem(), deleteMenu));
+
+			this.getListView().setOnKeyPressed(
+					event -> {
+						if (event.getCode() == KeyCode.C
+								&& event.isControlDown()) {
+							String selectedKey = getListView()
+									.getSelectionModel().getSelectedItem();
+							copy(selectedKey);
+						}
+					});
 		}
 	}
 
@@ -60,5 +83,24 @@ public class MessageKeyListCell extends ListCell<String> {
 			}
 		});
 		return deleteMenu;
+	}
+
+	private MenuItem getCopyMenu(String messageKey) {
+		MenuItem copyMenu = new MenuItem(MessageReader.getInstance()
+				.getMessage("action.copyMessageKey"));
+		copyMenu.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				copy(messageKey);
+			}
+
+		});
+		return copyMenu;
+	}
+
+	private void copy(String messageKey) {
+		logger.info("Copy key : " + messageKey);
+		Toolkit.getDefaultToolkit().getSystemClipboard()
+				.setContents(new StringSelection(messageKey), null);
 	}
 }
